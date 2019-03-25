67d0e1a315339debe921603cdef635543d8e35ca
Lorenatacal <Lorenatacal@gmail.com>
2019-03-25 23:27:09 +0000

    Create header for the app

--- src/App.js
+++ src/App.js
@@ -1,5 +1,4 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
@@ -7,18 +6,8 @@ class App extends Component {
     return (
       <div className="App">
         <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
+          <img src="http://www.funretrospectives.com/wp-content/uploads/2015/12/rock-paper-scissors.png" className="App-logo" alt="logo" />
+          <h2>Play Rock Paper Scissors</h2>
         </header>
       </div>
     );
