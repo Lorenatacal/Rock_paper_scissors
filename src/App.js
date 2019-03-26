68df479a4fcd3c0b88246abb8a54ab371f9a8d70
Lorenatacal <Lorenatacal@gmail.com>
2019-03-26 23:44:08 +0000

    Create ComputerScreen component

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import Header from './Header.js';
 import UserScreen from './UserScreen.js';
+import ComputerScreen from './ComputerScreen.js';
 import './App.css';
 
 class App extends Component {
@@ -9,6 +10,7 @@ class App extends Component {
       <div className="App">
         <Header />
         <UserScreen />
+        <ComputerScreen />
       </div>
     );
   }
---
+++ src/ComputerScreen.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+import './ComputerScreen.css';
+
+class ComputerScreen extends Component {
+    render() {
+        return (
+            <div class= "split right">
+                <p> Computer's Options</p>
+            </div>
+        );
+    }
+}
+export default ComputerScreen;
