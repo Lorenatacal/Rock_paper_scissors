182e1b2f5b5ff553419732a2c89ec97f01d87b9d
Lorenatacal <Lorenatacal@gmail.com>
2019-03-26 22:53:29 +0000

    Create Header

--- src/App.js
+++ src/App.js
@@ -1,14 +1,12 @@
 import React, { Component } from 'react';
+import Header from './Header.js';
 import './App.css';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src="http://www.funretrospectives.com/wp-content/uploads/2015/12/rock-paper-scissors.png" className="App-logo" alt="logo" />
-          <h2>Play Rock Paper Scissors</h2>
-        </header>
+        <Header />
       </div>
     );
   }
