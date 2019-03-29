78e5bbcedd1a7088c645c07c4666ff03bf987ef9
Lorenatacal <Lorenatacal@gmail.com>
2019-03-29 10:24:27 +0000

    Create handleChange that shows the name of a new player

--- src/AddName.js
+++ src/AddName.js
@@ -1,12 +1,29 @@
 import React, { Component } from "react";
 import styled from "styled-components";
 
+// const StyledText = styled.p`
+//   visibility: hidden;
+// `;
+
 class AddName extends Component {
   constructor(props) {
     super(props);
     this.state = {
       name: ""
     };
+    this.handleChange = this.handleChange.bind(this);
+    this.handleSubmit = this.handleSubmit.bind(this);
+  }
+
+  handleChange(e) {
+    this.setState({
+      name: e.target.value
+    });
+  }
+
+  handleSubmit(e) {
+    e.preventdefault();
+    this.setState({});
   }
   render() {
     return (
@@ -14,11 +31,14 @@ class AddName extends Component {
         <form>
           <input
             type="text"
-            onChange={this.handleChange}
             placeholder="Type your name here"
+            onChange={this.handleChange}
           />
-          <input type="submit" onSubmit={this.handleSubmit} value="Submit" />
+          <button type="submit" onSubmit={this.handleChange}>
+            Submit
+          </button>
         </form>
+        <p>Wecome to the game {this.state.name}</p>
       </div>
     );
   }
