065a76eb039c2639bb71b0e140bae09f5ffa0346
Lorenatacal <Lorenatacal@gmail.com>
2019-04-05 15:01:20 +0100

    Pass test choseWinner(userValue, computerValue) to return draw if both computer and user chose scissor

--- src/game-engine/gameEngine.js
+++ src/game-engine/gameEngine.js
@@ -2,4 +2,7 @@ export default function choseWinner(userValue, computerValue) {
   if ((userValue === "paper") & (computerValue === "paper")) {
     return "Draw";
   }
+  if ((userValue === "scissor") & (computerValue === "scissor")) {
+    return "Draw";
+  }
 }
