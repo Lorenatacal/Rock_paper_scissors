fa0078034ab9e3d25af142718b4ffd244de46523
Lorenatacal <Lorenatacal@gmail.com>
2019-04-05 14:57:52 +0100

    Test choseWinner(userValue, computerValue) to return draw if both computer and user chose scissor

--- src/game-engine/gameEngine.test.js
+++ src/game-engine/gameEngine.test.js
@@ -1,11 +1,19 @@
 import choseWinner from "./gameEngine";
 
-test("choseWinner() to be defined", () => {
-  expect(typeof choseWinner).toBe("function");
-});
-test("choseWinner() should console.log Draw if both user and computer chose paper", () => {
-  let userValue = "paper";
-  let computerValue = "paper";
-  let winner = choseWinner(userValue, computerValue);
-  expect(winner).toEqual("Draw");
+describe("choseWinner()", () => {
+  test("to be defined", () => {
+    expect(typeof choseWinner).toBe("function");
+  });
+  test("should return Draw if both user and computer chose paper", () => {
+    let userValue = "paper";
+    let computerValue = "paper";
+    let winner = choseWinner(userValue, computerValue);
+    expect(winner).toEqual("Draw");
+  });
+  test("should return Draw if both user and computer chose scissor", () => {
+    let userValue = "scissor";
+    let computerValue = "scissor";
+    let winner = choseWinner(userValue, computerValue);
+    expect(winner).toEqual("Draw");
+  });
 });
