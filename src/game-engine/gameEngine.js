8ccef593b9e23e8049ebc6d3a55e5993ae88c37e
Lorenatacal <Lorenatacal@gmail.com>
2019-04-05 14:47:15 +0100

    Pass test ChoseWinner(userValue, computerValue) should return Draw if both user and computer chose paper

--- src/game-engine/gameEngine.js
+++ src/game-engine/gameEngine.js
@@ -1,1 +1,5 @@
-export default function choseWinner(userValue, computerValue) {}
+export default function choseWinner(userValue, computerValue) {
+  if ((userValue === "paper") & (computerValue === "paper")) {
+    return "Draw";
+  }
+}
