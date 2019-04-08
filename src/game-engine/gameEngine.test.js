494833f69748e82410368ef8682e4ce179abf8f0
Lorenatacal <Lorenatacal@gmail.com>
2019-04-08 18:01:49 +0100

    Test choseWinner to return Computer won when user choses rock and computer paper

--- src/game-engine/gameEngine.test.js
+++ src/game-engine/gameEngine.test.js
@@ -52,4 +52,10 @@ describe("choseWinner()", () => {
     let winner = choseWinner(userValue, computerValue);
     expect(winner).toEqual("Computer won");
   });
+  test("should return 'Computer won' if computer choses scissor and user paper", () => {
+    let userValue = "rock";
+    let computerValue = "paper";
+    let winner = choseWinner(userValue, computerValue);
+    expect(winner).toEqual("Computer won");
+  });
 });
