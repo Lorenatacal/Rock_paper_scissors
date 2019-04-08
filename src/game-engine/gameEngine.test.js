2eec7d964ca30deee0d87baa27ab15a6827691d3
Lorenatacal <Lorenatacal@gmail.com>
2019-04-08 17:44:13 +0100

    Test choseWinner to return Computer won when user choses scissor and computer rock

--- src/game-engine/gameEngine.test.js
+++ src/game-engine/gameEngine.test.js
@@ -22,22 +22,28 @@ describe("choseWinner()", () => {
     let winner = choseWinner(userValue, computerValue);
     expect(winner).toEqual("Draw");
   });
-  test("should return 'User won' if user choses rock and user scissor", () => {
+  test("should return 'User won' if user choses rock and computer scissor", () => {
     let userValue = "rock";
     let computerValue = "scissor";
     let winner = choseWinner(userValue, computerValue);
     expect(winner).toEqual("User won");
   });
-  test("should return 'User won' if user choses scissor and user paper", () => {
+  test("should return 'User won' if user choses scissor and computer paper", () => {
     let userValue = "scissor";
     let computerValue = "paper";
     let winner = choseWinner(userValue, computerValue);
     expect(winner).toEqual("User won");
   });
-  test("should return 'User won' if user choses paper and user rock", () => {
+  test("should return 'User won' if user choses paper and computer rock", () => {
     let userValue = "paper";
     let computerValue = "rock";
     let winner = choseWinner(userValue, computerValue);
     expect(winner).toEqual("User won");
+  });
+  test("should return 'Computer won' if computer choses rock and user scissor", () => {
+    let userValue = "scissor";
+    let computerValue = "rock";
+    let winner = choseWinner(userValue, computerValue);
+    expect(winner).toEqual("Computer won");
   });
 });
