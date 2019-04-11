0881bd401db3a84629136b28dddf9cb8edae53f2
Lorenatacal <Lorenatacal@gmail.com>
2019-04-11 22:47:17 +0100

    Refactor gameEngine tests

--- src/game-engine/gameEngine.test.js
+++ src/game-engine/gameEngine.test.js
@@ -1,61 +1,64 @@
 import choseWinner from "./gameEngine";
 
 describe("choseWinner()", () => {
+  let userValue;
+  let computerValue;
+  let winner;
   test("to be defined", () => {
     expect(typeof choseWinner).toBe("function");
   });
   test("should return Draw if both user and computer chose paper", () => {
-    let userValue = "paper";
-    let computerValue = "paper";
-    let winner = choseWinner(userValue, computerValue);
+    userValue = "paper";
+    computerValue = "paper";
+    winner = choseWinner(userValue, computerValue);
     expect(winner).toEqual("Draw");
   });
   test("should return Draw if both user and computer chose scissor", () => {
-    let userValue = "scissor";
-    let computerValue = "scissor";
-    let winner = choseWinner(userValue, computerValue);
+    userValue = "scissor";
+    computerValue = "scissor";
+    winner = choseWinner(userValue, computerValue);
     expect(winner).toEqual("Draw");
   });
   test("should return Draw if both user and computer chose rock", () => {
-    let userValue = "rock";
-    let computerValue = "rock";
-    let winner = choseWinner(userValue, computerValue);
+    userValue = "rock";
+    computerValue = "rock";
+    winner = choseWinner(userValue, computerValue);
     expect(winner).toEqual("Draw");
   });
   test("should return 'User won' if user choses rock and computer scissor", () => {
-    let userValue = "rock";
-    let computerValue = "scissor";
-    let winner = choseWinner(userValue, computerValue);
+    userValue = "rock";
+    computerValue = "scissor";
+    winner = choseWinner(userValue, computerValue);
     expect(winner).toEqual("User won");
   });
   test("should return 'User won' if user choses scissor and computer paper", () => {
-    let userValue = "scissor";
-    let computerValue = "paper";
-    let winner = choseWinner(userValue, computerValue);
+    userValue = "scissor";
+    computerValue = "paper";
+    winner = choseWinner(userValue, computerValue);
     expect(winner).toEqual("User won");
   });
   test("should return 'User won' if user choses paper and computer rock", () => {
-    let userValue = "paper";
-    let computerValue = "rock";
-    let winner = choseWinner(userValue, computerValue);
+    userValue = "paper";
+    computerValue = "rock";
+    winner = choseWinner(userValue, computerValue);
     expect(winner).toEqual("User won");
   });
   test("should return 'Computer won' if computer choses rock and user scissor", () => {
-    let userValue = "scissor";
-    let computerValue = "rock";
-    let winner = choseWinner(userValue, computerValue);
+    userValue = "scissor";
+    computerValue = "rock";
+    winner = choseWinner(userValue, computerValue);
     expect(winner).toEqual("Computer won");
   });
   test("should return 'Computer won' if computer choses scissor and user paper", () => {
-    let userValue = "paper";
-    let computerValue = "scissor";
-    let winner = choseWinner(userValue, computerValue);
+    userValue = "paper";
+    computerValue = "scissor";
+    winner = choseWinner(userValue, computerValue);
     expect(winner).toEqual("Computer won");
   });
   test("should return 'Computer won' if computer choses paper and user rock", () => {
-    let userValue = "rock";
-    let computerValue = "paper";
-    let winner = choseWinner(userValue, computerValue);
+    userValue = "rock";
+    computerValue = "paper";
+    winner = choseWinner(userValue, computerValue);
     expect(winner).toEqual("Computer won");
   });
 });
