0d527dd65ce3b2d0acb7360ac138a80578ac7c3c
Lorenatacal <Lorenatacal@gmail.com>
2019-06-06 11:11:58 +0100

    Test gameWinner() to return -1 if we pass 'rockscissors' as argument

--- src/game-engine/ultraGameEngine.test.js
+++ src/game-engine/ultraGameEngine.test.js
@@ -3,3 +3,6 @@ import gameWinner from "./ultraGameEngine";
 test("gameWinner() should retun 1 if we pass 'scissorsrock' as argument", () => {
   expect(gameWinner("scissorsrock")).toEqual(1);
 });
+test("gameWinner() should return -1 if we pass 'rockscissors' as argument", () => {
+  expect(gameWinner("rockscissors")).toEqual(-1);
+});
