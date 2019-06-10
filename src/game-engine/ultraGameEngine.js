926d1666a6d0bcc3691b3fb986c405103b444cb3
Lorenatacal <Lorenatacal@gmail.com>
2019-06-10 23:39:31 +0100

    Pass test rules to render correctly

--- src/game-engine/ultraGameEngine.js
+++ src/game-engine/ultraGameEngine.js
@@ -1,8 +1,15 @@
+export const rules = {
+  scissorsrock: 1,
+  scissorspaper: 1,
+  paperrock: 1,
+  rockscissors: -1,
+  paperscissors: -1,
+  rockpaper: -1,
+  rockrock: 0,
+  scissorsscissors: 0,
+  paperpaper: 0
+};
+
 export default function gameWinner(option) {
-  let rules = {
-    scissorsrock: 1,
-    rockscissors: -1,
-    rockrock: 0
-  };
   return rules[option];
 }
--- src/game-engine/ultraGameEngine.test.js
+++ src/game-engine/ultraGameEngine.test.js
@@ -1,4 +1,7 @@
+import React, { Component } from "react";
 import gameWinner from "./ultraGameEngine";
+import { rules } from "./ultraGameEngine";
+import toJson from "enzyme-to-json";
 
 test("gameWinner() should retun 1 if we pass 'scissorsrock' as argument", () => {
   expect(gameWinner("scissorsrock")).toEqual(1);
@@ -8,4 +11,7 @@ test("gameWinner() should return -1 if we pass 'rockscissors' as argument", () =
 });
 test("gameWinner() should return 0 if we pass 'rockrock' as argument", () => {
   expect(gameWinner("rockrock")).toEqual(0);
+});
+test("rules to have correct shape", () => {
+  expect(rules).toMatchSnapshot();
 });
